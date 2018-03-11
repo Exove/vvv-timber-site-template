c2e81391435859605591a57967c8b439d23a9ab4
AleksandrBeliaev <aleksandr.beliaev@exove.ee>
2018-03-11 22:44:29 +0200

    Set the ACF_PRO_KEY var as one-off variable when doing composer install

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -4,9 +4,6 @@
 # Configuration
 CONFIG_ACF_PRO_KEY=`get_config_value 'acf_pro_key'`
 
-# Add environment vars to the VM
-noroot export ACF_PRO_KEY=${CONFIG_ACF_PRO_KEY}
-
 # Make a database, if we don't already have one
 echo -e "\nCreating database 'sitename' (if it's not already there)"
 mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS sitename"
@@ -42,7 +39,7 @@ PHP
   chown -R www-data:www-data /home/vagrant/.composer
   echo "Installing project composer dependencies..."
   cd ${VVV_PATH_TO_SITE}/site
-  noroot sudo -u www-data /usr/local/bin/composer install
+  noroot sudo -u www-data ACF_PRO_KEY=${CONFIG_ACF_PRO_KEY} /usr/local/bin/composer install
   echo "Symlinking wp-content..."
   # removing the vanilla wp-content folder installed with WP core
   rm -rf ${VVV_PATH_TO_SITE}/public_html/wp-content
@@ -64,7 +61,7 @@ else
   chown -R www-data:www-data /home/vagrant/.composer
   echo "Installing project composer dependencies..."
   cd ${VVV_PATH_TO_SITE}/site
-  noroot sudo -u www-data /usr/local/bin/composer install
+  noroot sudo -u www-data ACF_PRO_KEY=${CONFIG_ACF_PRO_KEY} /usr/local/bin/composer install
   echo "Removing themes installed during earlier provision..."
   rm -rf wp-content/themes
   echo "Activating plugins..."
